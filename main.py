98273144fe6fdaf21ca6a97bc10dcda76bce1dc3
Iceplanet <isshin.20247@gmail.com>
2025-03-08 04:48:07 +0900

    query parameter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Union
 from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,6 +9,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 # クエリパラメータとパスパラメータで名前が被ってもよい
 @app.get("/users/{user_id}/items/{item_id}")
